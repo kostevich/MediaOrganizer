7409b68986e62811af30847d65ce6e4cc68ef863
Irisha <irisha.kostevich@mail.ru>
2023-10-18 21:32:15 +0300

    New: MovingMediaFiles
    Name of folders will be added in list, name of files in dict.

--- Main.py
+++ Main.py
@@ -17,5 +17,25 @@ def CreateNewFolder():
             if not os.path.exists(f'C:/Data storage/Photos/From phone/{NameNewFolder}'):
                 os.makedirs(f'C:/Data storage/Photos/From phone/{NameNewFolder}')
 
-CreateNewFolder()
 
+def MovingMediaFiles():
+    NameMediaFiles = dict()
+    NameFolders = list()
+    for UnitContent in Content:
+        if os.path.isdir(f'C:/Data storage/Photos/From phone/{UnitContent}'):
+            NameFolders.append(UnitContent)
+            print(NameFolders)
+        if UnitContent.endswith('jpg') or UnitContent.endswith('mp4'):
+            BreakdownUnitContent = UnitContent.split('.')
+            NameDataMediaFiles = BreakdownUnitContent[0]
+            SuffixesMediaFiles = BreakdownUnitContent[1]
+            NameMediaFiles[NameDataMediaFiles]= SuffixesMediaFiles
+            for NameFile in NameMediaFiles.keys():
+                if NameFile in NameFolders:
+                    print(f"{NameMediaFiles.keys}{NameMediaFiles.values}", f"{NameMediaFiles.keys}/{NameMediaFiles.keys}{NameMediaFiles.values}")
+                    os.replace(f"{NameMediaFiles.keys}{NameMediaFiles.values}", f"{NameMediaFiles.keys}/{NameMediaFiles.keys}{NameMediaFiles.values}")
+        
+
+
+CreateNewFolder()
+MovingMediaFiles()
